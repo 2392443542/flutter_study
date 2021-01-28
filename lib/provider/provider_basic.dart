5fd3d627c6a1be5add3a3332935f21c205ee7bdc
alieen lee <alieen2392443542@gmail.com>
2021-01-28 18:00:46 +0800

    provider

---
+++ lib/provider/provider_basic.dart
@@ -0,0 +1,100 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'CounterViewModel.dart';
+
+void main() {
+
+  runApp( ChangeNotifierProvider(child: MyApp(),create:(ctx) => CounterViewModel(),));
+
+  // return
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body:Center(
+        child:Column(
+          children: [CounterDemo1(),CounterDemo2()],
+        ) ,
+      ),
+      floatingActionButton: Selector<CounterViewModel,CounterViewModel>(selector: (context,  vm){
+      return  vm;
+     },
+        shouldRebuild:(pre,next)=>false,
+        builder: (ctx,vm,child){
+        return FloatingActionButton(child:child,onPressed: (){
+        vm.counter++;
+        },);
+      },
+        child: Icon(Icons.add),
+      ),
+        //
+    );
+  }
+}
+
+class CounterDemo1 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    int counter = Provider.of <CounterViewModel>(context).counter;
+    return Container(
+      color: Colors.red,
+      child: Text("$counter",style: TextStyle(
+          fontSize: 14
+      ),),
+    );
+  }
+}
+
+class CounterDemo2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      // child: Consumer<counter>(
+        child:Consumer<CounterViewModel>(builder: (ctx, vm, child){
+          return  Text("${vm.counter}",style: TextStyle(
+            fontSize: 14,
+          ),);
+        },)
+      // )
+    );
+  }
+}
+
---
+++ lib/provider/viewModel/user_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class userViewModel extends ChangeNotifier {
+  String name;
+  String userId;
+  int level;
+
+}
